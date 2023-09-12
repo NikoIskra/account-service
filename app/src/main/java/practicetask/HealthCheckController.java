86e42838da6d207f6255933cc04dd804da761d01
Niko Iskra <niskra121@gmail.com>
2023-09-12 08:03:19 +0200

    changed healtcheck API endpoint

--- app/src/main/java/practicetask/HealthCheckController.java
+++ app/src/main/java/practicetask/HealthCheckController.java
@@ -2,11 +2,13 @@ package practicetask;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HealthCheckController {
-    @GetMapping()
+    @GetMapping("/healthcheck")
     public ResponseEntity practiceResponse() {
         return ResponseEntity.ok().build();
     }
